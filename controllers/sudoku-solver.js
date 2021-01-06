925a11921cd88fd1079e789cee17e32de1932f50
Laurent Labine <laurentlabine@gmail.com>
2021-01-07 00:14:13 +0100

    Cleaned Code. Checking in with only challenges left

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -18,53 +18,49 @@ class SudokuSolver {
   }
 
   check(puzStr, coordinate, value) {
-      var row, col
-      var res = {}
-      this.status = {}
+    var row, col
+    var res = {}
+    this.status = {}
 
-      if (!(puzStr !== undefined) || (!(coordinate !== undefined)) || (!(value !== undefined)))
-        return this.status = {error : "Required field(s) missing" }
+    if (!(puzStr !== undefined) || (!(coordinate !== undefined)) || (!(value !== undefined)))
+      return this.status = {error : "Required field(s) missing" }
 
-      this.validate(puzStr)
+    this.validate(puzStr)
 
-      if(this.status.error)//If validation flags errors, we stop the check here and return the error.  Otherwise we keep going
-        return this.status
-
-      if(!/^[a-iA-I]\d{1}$/.test(coordinate))//Coordinate invalid
-        return this.status = {error: "Invalid coordinate"}
-
-      row = coordinate.split('')[0].toUpperCase()
-      col = coordinate.split('')[1]
-
-      if(!/^\d{1}$/.test(value))//Value not between 0-9
-        return this.status = {error : "Invalid value"}
-
-        this.status = { "valid": true, "conflict": [] }
-
-      //if every input is valid, we start looking into placement
-
-      //console.log("puz : " + puzStr + " row : " + row + " col : " + col + " value : " + value)
-
-      this.checkColPlacement(puzStr,row,col,value)
-
-      this.checkRowPlacement(puzStr,row,col,value)
-
-      this.checkRegionPlacement(puzStr,row,col,value)
-
-      if(this.status.conflict.length > 0)
-        this.status.valid = false
-      else
-        this.status = {valid:true}
-
+    if(this.status.error)//If validation flags errors, we stop the check here and return the error.  Otherwise we keep going
       return this.status
+
+    if(!/^[a-iA-I]\d{1}$/.test(coordinate))//Coordinate invalid
+      return this.status = {error: "Invalid coordinate"}
+
+    row = coordinate.split('')[0].toUpperCase()
+    col = coordinate.split('')[1]
+
+    if(!/^\d{1}$/.test(value))//Value not between 0-9
+      return this.status = {error : "Invalid value"}
+
+    this.status = { "valid": true, "conflict": [] }
+
+    this.checkColPlacement(puzStr,row,col,value)
+
+    this.checkRowPlacement(puzStr,row,col,value)
+
+    this.checkRegionPlacement(puzStr,row,col,value)
+
+    if(this.status.conflict.length > 0)
+      this.status.valid = false
+    else
+      this.status = {valid:true}
+
+    return this.status
   }
 
   checkRowPlacement(puzzleString, row, column, value) {
-      var startIndex = 0 + this.convert[row] * 9 //Index of the first element of that row.  First row index 0 to 8, second 9 to 17, etc...
-      var row = puzzleString.slice(startIndex, startIndex + 9)
+    var startIndex = 0 + this.convert[row] * 9 //Index of the first element of that row.  First row index 0 to 8, second 9 to 17, etc...
+    var row = puzzleString.slice(startIndex, startIndex + 9)
 
-      if (row.indexOf(value) > -1)
-        this.status.conflict.push("row")
+    if (row.indexOf(value) > -1)
+      this.status.conflict.push("row")
   }
 
   checkColPlacement(puzzleString, row, column, value) {
@@ -86,45 +82,45 @@ class SudokuSolver {
     if(puzzleString.substr(index,1) !== ".")//If there is already a value there, conflict!
       return this.status.conflict.push("region")
 
-      var colInd, rowInd, upperColInd, upperRowInd
-      var regArr = []
+    var colInd, rowInd, upperColInd, upperRowInd
+    var regArr = []
 
-      //Region Row lower Limits
-      var regRow = this.convert[row]/3
-      var regCol = parseFloat(column)/3
+    //Region Row lower Limits
+    var regRow = this.convert[row]/3
+    var regCol = parseFloat(column)/3
 
-      //Determine Start column of the region of the value
-      if(regCol>2)
-        colInd = 6
-      else if(regCol > 1)
-        colInd = 3
-      else
-        colInd = 0
+    //Determine Start column of the region of the value
+    if(regCol>2)
+      colInd = 6
+    else if(regCol > 1)
+      colInd = 3
+    else
+      colInd = 0
 
-      //Determine Start column of the region of the value
-      if(regRow>=2)
-        rowInd = 6
-      else if(regRow >=1)
-        rowInd = 3
-      else
-        rowInd = 0
+    //Determine Start column of the region of the value
+    if(regRow>=2)
+      rowInd = 6
+    else if(regRow >=1)
+      rowInd = 3
+    else
+      rowInd = 0
 
-      //Determine upperlimit Indexes
-      upperColInd = colInd + 3
-      upperRowInd = rowInd + 3        
+    //Determine upperlimit Indexes
+    upperColInd = colInd + 3
+    upperRowInd = rowInd + 3        
 
-      //Extract region
-      for(var j = rowInd; j<upperRowInd; j++){//Row 
-        for(var i = colInd; i < upperColInd; i++){//Column
-          regArr.push(puzzleString.substr(i + 9*j,1))
-        }
+    //Extract region
+    for(var j = rowInd; j<upperRowInd; j++){//Row 
+      for(var i = colInd; i < upperColInd; i++){//Column
+        regArr.push(puzzleString.substr(i + 9*j,1))
       }
+    }
 
-      //Check if value already present in region
-      if(regArr.includes(value))
-        return this.status.conflict.push("region")
-      else
-        return
+    //Check if value already present in region
+    if(regArr.includes(value))
+      return this.status.conflict.push("region")
+    else
+      return
   }
 
   getKeyByValue(value) {
@@ -134,8 +130,7 @@ class SudokuSolver {
   solve(puzzleString) {
 
     this.status = {}
-    var possible = []//Array of array holding possibilities
-    var puzzleArr = []//Array version of the array's string to allow
+    var potential = []//Array of array holding possibilities
     var ind = 0
     var coordinate
     var foundOne
@@ -149,13 +144,6 @@ class SudokuSolver {
 
     if(this.status.error)//If validation flags errors, we stop the check here and return the error.  Otherwise we keep going
       return this.status
-
-    //for A1 to I9 check all the valuesfrom 0 to 9 and keep in a possibleMatch array every ones that doesnt return an error when checked
-
-    //Foreach values in the array, test numbers from 0 to 9 and save in an potential candidates array the possible ones.
-
-
-
 
     //Validating that puzzle entered is valid
     for(var i = 0; i < 9 ; i++){//9 rows
@@ -172,70 +160,43 @@ class SudokuSolver {
         count++
       }
     }
-
-    console.log("Puzzle solvable!")
-    console.log(puzzleString)
-    count = 0;
     
     //Creating an array of 9x9x9 in order to store the prospects for each square.
-    for(var i = 0; i< 9; i++){
-       possible[i] = new Array(9) 
-       puzzleArr[i] = new Array(9)
+    for(var i = 0; i< 9; i++)
+    potential[i] = new Array(9) 
+
+    foundOne = true
+
+    while(foundOne){
+      foundOne = 0
+      count  = 0
+
+      //every iteration of the puzzle we reset possible results to re evaluate
+      for(var i = 0; i< 9; i++){
+        for(var j = 0; j< 9; j++ ){
+          potential[i][j] = new Array
+        }
+      }
+
+      for(var i = 0; i < 9 ; i++){//9 rows
+        for(var j = 0; j < 9; j++){//9 Columns
+          ind = j+1 //My system is 0 based but the rest of the code takes A1 to A9 so i<m adjusting this here
+          coordinate = this.getKeyByValue(i) + ind//Forming coordinate in a format the validate functions understands.
+          count++ //Keeping a count so I know where to place the value found in the string.
+
+          for(var k = 1; k < 10; k++)            
+            if(this.check(puzzleString,coordinate,k.toString()).valid)//Evaluate 1-9 in every locations available locations
+              potential[i][j].push(k)//Saves values that aare potential matches
+
+            if(potential[i][j].length == 1){//Found Only One Possibility.  Replacing it in puzzleString and setting flag for another go.
+              puzzleString = puzzleString.substr(0,count-1) + potential[i][j][0].toString() + puzzleString.substr(count)
+              foundOne = true//As long as we find new match we keep going.
+            }
+        }
+      }
+      console.log(puzzleString)//Printing result after each iteration
     }
-
-    //copying the string to an array
-    for(var i = 0; i< 9; i++){
-      for(var j = 0; j< 9; j++ ){
-      puzzleArr[i][j] = puzzleString.substr(count,1)
-      count++
-      }
-   }
-
-    count = 0;
-
-do{
-  foundOne = 0
-
-  //every iteration of the puzzle we reset possible results to re evaluate
-  for(var i = 0; i< 9; i++){
-    for(var j = 0; j< 9; j++ ){
-    possible[i][j] = new Array
-    count++
-      }
-  }
-
-  for(var i = 0; i < 9 ; i++){//9 rows
-    for(var j = 0; j < 9; j++){//9 Columns
-      ind = j+1
-      coordinate = this.getKeyByValue(i) + ind
-
-      for(var k = 1; k < 10; k++)            
-        if(this.check(puzzleString,coordinate,k.toString()).valid)//the rest of my logic is based on the puzzle as a string so converting the number to a string for comparison.
-          possible[i][j].push(k)
-
-      if(possible[i][j].length == 1){
-        puzzleArr[i][j] = possible[i][j][0]//1 because 0 is coordinate
-        foundOne = true
-      }
-    }
-  }
-
-  puzzleString = ""
-
-  //Replace the String with the Array resulting the pass as the rest of the treatment relies on the string format
-  for(var i = 0; i< 9; i++)
-    for(var j = 0; j< 9; j++ )
-        puzzleString += puzzleArr[i][j]
-
-  console.log(puzzleString)//Printing result after each iteration
-
-  }while(foundOne)
-
-  if(puzzleString.indexOf(".") !== -1)//Solution wasnt found
-    return { error: 'Puzzle cannot be solved' }
-  else
-    return { solution : puzzleString}
-
+  return { solution : puzzleString}
   }
 }
 
